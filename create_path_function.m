761ecc3032750964472da8b37a33ec3c7f2eddbf
Glorb456 <glorb458@gmail.com>
2026-04-30 21:04:59 -0400

    implemented first person view

--- create_path_function.m
+++ create_path_function.m
@@ -16,9 +16,10 @@ function path = create_path_function(X,Y,Z)
     colormap summer; lighting gouraud; camlight;
     hold on;
     grid on;
-    view(3); 
+    camproj('perspective');
+    camup([0 0 1]);
+    camva(75); 
     axis equal;
-    zlim([-35 35]);
     start_z = interp2(X, Y, Z, 0, 0);
     hLine = animatedline('Color', 'b', 'LineWidth', 3);
     hMarker = plot3(0, 0, start_z, 'r*', 'MarkerSize', 12, 'LineWidth', 2); 
@@ -30,10 +31,16 @@ function path = create_path_function(X,Y,Z)
     
     curr_x = 0;
     curr_y = 0; 
-    addpoints(hLine, curr_x, curr_y)
+    addpoints(hLine, curr_x, curr_y, start_z)
     
     
-    path = [0;0];
+    path = [0;0;0];
+    pitch = 0; 
+    yaw = 90;
+    eye_height = .8; 
+    look_speed = 2; 
+    look_dist = 5; 
+    curr_z = start_z; 
     
     while true
         % Read the current state of the controller
@@ -45,27 +52,63 @@ function path = create_path_function(X,Y,Z)
         if buttons(2) == 1
             speed = .05;
         end 
+
+        if abs(axes(4)) > 0.05
+            yaw = yaw - axes(4) * look_speed; 
+        end 
+        if abs(axes(5)) > .05
+            pitch = pitch - axes(5) *look_speed;
+            pitch = max(min(pitch, 85), -85); 
+        end 
+
     
     
         if sqrt(axes(1).^2+axes(2).^2) > .05
-            the_angle = atan2d(-axes(2), -axes(1)) - 90;
-            curr_y = curr_y +  speed*cosd(the_angle);
-            curr_x = curr_x + speed*sind(the_angle);
+            % the_angle = atan2d(-axes(2), -axes(1)) - 90;
+            % curr_y = curr_y +  speed*cosd(the_angle);
+            % curr_x = curr_x + speed*sind(the_angle);
+            % curr_z = interp2(X, Y, Z, curr_x, curr_y);
+            move_fwd = -axes(2)*speed; 
+            move_side = axes(1)*speed; 
+
+            dx = (move_fwd*cosd(yaw))+(move_side*cosd(90-yaw)); 
+            dy = (move_fwd*sind(yaw))+(move_side*sind(90-yaw));
+
+            curr_x = curr_x + dx;
+            curr_y = curr_y + dy;
+            curr_x = max(min(curr_x, 10), -10);
+            curr_y = max(min(curr_y, 10), -10);
             curr_z = interp2(X, Y, Z, curr_x, curr_y);
-            path(:, end+1) = [curr_x; curr_y];
+
+
+
+
     
-            disp(the_angle)
             addpoints(hLine, curr_x, curr_y, curr_z)
             hMarker.XData = curr_x;
             hMarker.YData = curr_y;
             hMarker.ZData = curr_z;
-            drawnow limitrate; 
-            disp(the_angle)
+
+            
+            path(:, end+1, :) = [curr_x; curr_y; curr_z ];
+
         else 
             disp(0)
         end 
+        campos([curr_x, curr_y, curr_z + eye_height]);
+        target_x = curr_x + look_dist*cosd(pitch)*cosd(yaw);
+        target_y = curr_y + look_dist*cosd(pitch)*sind(yaw);
+        target_z = curr_z + eye_height + look_dist*sind(pitch); 
+        camtarget([target_x, target_y, target_z])
+        drawnow limitrate; 
+
+
+        
         if buttons(4) == 1
             disp('Exiting...');
+            break;
+        end 
+        if sqrt((curr_x-10).^2 + (curr_y-10).^2) < .5
             break;
         end 
         speed = .1;
@@ -76,6 +119,6 @@ function path = create_path_function(X,Y,Z)
             return
         end 
     end
-    plot(path(1, :), path(2,:), 'Color','g', 'LineWidth', 2)
+    plot3(path(1, :), path(2,:), path(3,:), 'Color','g', 'LineWidth', 2)
 end 
 
