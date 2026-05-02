8f5cace96bd04a7439b1f0b803258325b582f08f
JDS6666 <dju@olin.edu>
2026-05-02 14:39:43 -0400

    Add live route minimap

--- create_path_function.m
+++ create_path_function.m
@@ -1,5 +1,5 @@
 
-function path = create_path_function(X,Y,Z)
+function path = create_path_function(X,Y,Z,best_path)
 
     %with controller connected, move with left joystick, press A to speed
     %up, B to slow down, Y to exit/complete
@@ -9,23 +9,48 @@ function path = create_path_function(X,Y,Z)
     % buttons(1) -> A, buttons(2) -> B, buttons(3) -> X, buttons(4) -> Y
     
 
+    if nargin < 4
+        best_path = [];
+    end
+
     joy = sim3d.io.Joystick(ID=1);
-    
-    figure('Name', 'Rover Mission: Driving Phase');
-    surf(X, Y, Z, 'EdgeColor', 'none', 'FaceAlpha', 0.8); 
-    colormap summer; lighting gouraud; camlight;
-    hold on;
-    grid on;
-    camproj('perspective');
-    camup([0 0 1]);
-    camva(75); 
-    axis equal;
+
+    mission_fig = figure('Name', 'Rover Mission: Driving Phase');
+    ax3d = axes('Parent', mission_fig, 'Position', [0.05 0.06 0.9 0.9]);
+    surf(ax3d, X, Y, Z, 'EdgeColor', 'none', 'FaceAlpha', 0.8);
+    colormap(mission_fig, summer); lighting(ax3d, 'gouraud'); camlight(ax3d);
+    hold(ax3d, 'on');
+    grid(ax3d, 'on');
+    camproj(ax3d, 'perspective');
+    camup(ax3d, [0 0 1]);
+    camva(ax3d, 75);
+    axis(ax3d, 'equal');
     start_z = interp2(X, Y, Z, 0, 0);
-    hLine = animatedline('Color', 'b', 'LineWidth', 3);
-    hMarker = plot3(0, 0, start_z, 'r*', 'MarkerSize', 12, 'LineWidth', 2); 
-    
+    hLine = animatedline(ax3d, 'Color', 'b', 'LineWidth', 3);
+    hMarker = plot3(ax3d, 0, 0, start_z, 'r*', 'MarkerSize', 12, 'LineWidth', 2);
+
     goal_z = interp2(X, Y, Z, 10, 10);
-    plot3(10, 10, goal_z, 'p', 'MarkerSize', 20, 'MarkerFaceColor', 'red');
+    plot3(ax3d, 10, 10, goal_z, 'p', 'MarkerSize', 20, 'MarkerFaceColor', 'red');
+    if ~isempty(best_path)
+        plot3(ax3d, best_path(1, :), best_path(2, :), best_path(3, :) + 0.45, ...
+            'm--', 'LineWidth', 2);
+    end
+
+    map_ax = axes('Parent', mission_fig, 'Position', [0.055 0.065 0.26 0.26], ...
+        'Color', [0.96 0.98 0.94], 'Box', 'on');
+    contour(map_ax, X, Y, Z, 12, 'LineColor', [0.70 0.78 0.62]);
+    hold(map_ax, 'on');
+    axis(map_ax, 'equal');
+    axis(map_ax, [-10 10 -10 10]);
+    set(map_ax, 'XTick', [], 'YTick', [], 'Layer', 'top');
+    title(map_ax, '2D Route Map', 'FontSize', 9);
+    if ~isempty(best_path)
+        plot(map_ax, best_path(1, :), best_path(2, :), 'm--', 'LineWidth', 2);
+    end
+    map_player_line = plot(map_ax, 0, 0, 'b-', 'LineWidth', 2);
+    map_marker = plot(map_ax, 0, 0, 'ro', 'MarkerFaceColor', 'r', 'MarkerSize', 5);
+    plot(map_ax, 0, 0, 'go', 'MarkerFaceColor', 'g', 'MarkerSize', 5);
+    plot(map_ax, 10, 10, 'rp', 'MarkerFaceColor', 'r', 'MarkerSize', 7);
 
    
     
@@ -34,7 +59,7 @@ function path = create_path_function(X,Y,Z)
     addpoints(hLine, curr_x, curr_y, start_z)
     
     
-    path = [0;0;0];
+    path = [0;0;start_z];
     pitch = 0; 
     yaw = 90;
     eye_height = .8; 
@@ -44,7 +69,7 @@ function path = create_path_function(X,Y,Z)
     
     while true
         % Read the current state of the controller
-        [axes, buttons, ~] = read(joy);
+        [stick_axes, buttons, ~] = read(joy);
         speed = .1;
         if buttons(1) == 1
             speed = .2;
@@ -53,23 +78,23 @@ function path = create_path_function(X,Y,Z)
             speed = .05;
         end 
 
-        if abs(axes(4)) > 0.05
-            yaw = yaw - axes(4) * look_speed; 
+        if abs(stick_axes(4)) > 0.05
+            yaw = yaw - stick_axes(4) * look_speed;
         end 
-        if abs(axes(5)) > .05
-            pitch = pitch - axes(5) *look_speed;
+        if abs(stick_axes(5)) > .05
+            pitch = pitch - stick_axes(5) *look_speed;
             pitch = max(min(pitch, 85), -85); 
         end 
 
     
     
-        if sqrt(axes(1).^2+axes(2).^2) > .05
+        if sqrt(stick_axes(1).^2+stick_axes(2).^2) > .05
             % the_angle = atan2d(-axes(2), -axes(1)) - 90;
             % curr_y = curr_y +  speed*cosd(the_angle);
             % curr_x = curr_x + speed*sind(the_angle);
             % curr_z = interp2(X, Y, Z, curr_x, curr_y);
-            move_fwd = -axes(2)*speed; 
-            move_side = axes(1)*speed; 
+            move_fwd = -stick_axes(2)*speed;
+            move_side = stick_axes(1)*speed;
 
             dx = (move_fwd*cosd(yaw))+(move_side*cosd(90-yaw)); 
             dy = (move_fwd*sind(yaw))+(move_side*sind(90-yaw));
@@ -90,16 +115,18 @@ function path = create_path_function(X,Y,Z)
             hMarker.ZData = curr_z;
 
             
-            path(:, end+1, :) = [curr_x; curr_y; curr_z ]; %#ok<AGROW>
+            path(:, end+1) = [curr_x; curr_y; curr_z ]; %#ok<AGROW>
+            set(map_player_line, 'XData', path(1, :), 'YData', path(2, :));
+            set(map_marker, 'XData', curr_x, 'YData', curr_y);
 
         else 
             disp(0)
         end 
-        campos([curr_x, curr_y, curr_z + eye_height]);
+        campos(ax3d, [curr_x, curr_y, curr_z + eye_height]);
         target_x = curr_x + look_dist*cosd(pitch)*cosd(yaw);
         target_y = curr_y + look_dist*cosd(pitch)*sind(yaw);
         target_z = curr_z + eye_height + look_dist*sind(pitch); 
-        camtarget([target_x, target_y, target_z])
+        camtarget(ax3d, [target_x, target_y, target_z])
         drawnow limitrate; 
 
 
@@ -113,10 +140,10 @@ function path = create_path_function(X,Y,Z)
         end 
         pause(0.01); 
         if buttons(3) == 1
-            path = create_path_function(X, Y, Z);
+            path = create_path_function(X, Y, Z, best_path);
             return
         end 
     end
-    plot3(path(1, :), path(2,:), path(3,:), 'Color','g', 'LineWidth', 2)
+    plot3(ax3d, path(1, :), path(2,:), path(3,:), 'Color','g', 'LineWidth', 2)
 end 
 
